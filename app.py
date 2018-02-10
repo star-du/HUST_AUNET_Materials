9017d1739ba1e24c52d5ace026f872d0f57e3d44
smdsbz <694066681@qq.com>
2018-02-10 10:46:01 +0800

    viewed yesterdays work

--- app.py
+++ app.py
@@ -19,13 +19,13 @@ app.secret_key = 's\x1f}\xc8\xe29c\x84\xd1\x87P\x8e\xa5h5s\xf1\xfff\xcf\xfcK\xe8
 ######## user utils ########
 def verify(id,passwd):
     with sqlite3.connect(DATABASE) as database:
-        cursor = database.execute("select password from admin where id = ? ", (id,))
+        cursor = database.execute("SELECT PASSWORD FROM ADMIN WHERE ID = ? ", (id,))
         correct = cursor.fetchone()
-        if correct==None:  # wrong id
+        if correct == None:  # wrong id
             flash("用户名或密码错误！", category="error")
             return False
         else:  # correct id
-            if passwd ==correct[0]:
+            if passwd == correct[0]:
                 return True    # correct id-pass pair
             else:  # wrong passwd
                 flash("用户名或密码错误！", category="error")
@@ -49,7 +49,7 @@ def login():
                 assert session.pop('passwd', None) != None
             except:
                 pass
-            return redirect(url_for('personal'))
+            return redirect(url_for('personal'))    # TODO: redirect error
         else:
             session.pop('id', None)
             session.pop('passwd', None)
@@ -67,14 +67,15 @@ def logout():
         # clear session
     session.pop('id', None)
     print(session.pop('passwd', None))  # should get `None`
-    session.pop('filename', None)
+    # session.pop('filename', None)
     flash("已登出", category='message')
     return redirect(url_for('index'))
 ######## Miscellaneous entries ########
 
 @app.route('/opensource/')
 def opensource_info():
-    return render_template("opensource-info.html") # 此html可以直接沿用吧？
+    return render_template("opensource-info.html")  # 此html可以直接沿用吧？
+                                                    # REPLY: 可以
 
 
 ######## run ########
