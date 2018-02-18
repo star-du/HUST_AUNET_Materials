545cad01ecfd9a7fb5f0c2b7278231ecf3639864
muhoushaonian <32610114+muhoushaonian@users.noreply.github.com>
2018-02-18 22:13:31 +0800

    Create Email version2.0.py
    #1. 大家可以用这个试试，emm我运行时不知道为什么主题发送不出来
    #2.@star-du, 我定义了一个mail函数，然后你可以简单改造将mail_to设置为变量，从数据库中读取receivers.
    #3. 我们最后邮件用 text还是 html格式，需要加附件么？
    #4.是否需要考虑向管理员发送邮件（例如有新表格或者 出现了bug）

---
+++ Email version2.0.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from email.mime.multipart import MIMEMultipart
+from email.header import Header
+
+sender = '1457372615@qq.com' #发件人邮箱账号
+passWord = 'fjobxupauuwfighf'   #发件人邮箱授权码
+receivers = ['1516274933@qq.com']   #发件人的邮箱账号
+
+
+def mail():
+    ret = True
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = Header('华中大社团联合会', 'utf-8')
+        msg['To'] = Header('社联的小伙伴', 'utf-8')
+        subject = '来自社联的温馨提醒'
+        msg['Subject'] = Header(subject, 'utf-8')
+        msg = MIMEText('您已成功提交申请，请勿重复提交!', 'plain', 'utf-8')
+        s = smtplib.SMTP_SSL("smtp.qq.com",465) #第三方服务器及其端口
+        s.set_debuglevel(1)
+        s.login(sender,passWord)
+        #给列表中所有receiver发送邮件
+        for i in range(len(receivers)):
+            to = receivers[i]
+            msg['To'] = to
+            s.sendmail(sender,to,msg.as_string())
+            print('success')
+        s.quit()
+    except smtplib.SMTPException as e:
+        ret = False
+    return ret
+ret = mail()
+if ret:
+    print("success")
+else:
+    print("fail")
